7fff287e5d1c73f3e0c4de4cfcb9dced4ca4c4ce
Maximilian Mackh <mmackh@gmail.com>
2020-05-13 19:54:01 +0200

    removed macOS as a platform

--- Package.swift
+++ Package.swift
@@ -5,10 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "BaseComponents",
-    platforms: [
-        .macOS(.v10_15),
-        .iOS(.v11),
-    ],
+    platforms: [.iOS(.v11)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
